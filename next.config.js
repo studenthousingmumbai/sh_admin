642441aec83a3b574e5744f8ca9ffc380141e1c8
Tanay Kulkarni <tanaykulkarni7@gmail.com>
2023-02-04 20:08:07 +0530

    updated next config, set swcMinify to false

--- next.config.js
+++ next.config.js
@@ -1,7 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  swcMinify: true,
+  swcMinify: false,
   images: {
     domains: ['d22y2gqvewxrcl.cloudfront.net', 'google.com', 'i.pinimg.com', 'www.iconinc.co.uk', 'student-housing-test-bucket.s3.ap-south-1.amazonaws.com']
   }
